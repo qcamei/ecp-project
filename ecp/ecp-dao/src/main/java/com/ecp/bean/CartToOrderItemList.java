9d92798eebb8dd8a91b909a5924bc13ff2059f03
Hz1234 <pojo@foxmail.com>
2017-06-26 12:19:37 +0800

    修正结算前未选择购物车商品问题，提示用户

--- ecp/ecp-dao/src/main/java/com/ecp/bean/CartToOrderItemList.java
+++ ecp/ecp-dao/src/main/java/com/ecp/bean/CartToOrderItemList.java
@@ -2,8 +2,17 @@ package com.ecp.bean;
 
 import java.util.List;
 
+/**
+ * @ClassName CartToOrderItemList
+ * @Description 用于接收 购物车--->结算--->订单 数据 
+ * @author Administrator
+ * @Date 2017年6月26日 上午10:38:26
+ * @version 1.0.0
+ */
 public class CartToOrderItemList {
-private List<AddSkuToOrderBean> cartItemList;
+private long addrId;  //订单中用户收货地址ID
+private String buyerComment;  //订单备注
+private List<AddSkuToOrderBean> cartItemList;  //购物车中所选商品
 
 public List<AddSkuToOrderBean> getCartItemList() {
 	return cartItemList;
@@ -11,5 +20,21 @@ public List<AddSkuToOrderBean> getCartItemList() {
 
 public void setCartItemList(List<AddSkuToOrderBean> cartItemList) {
 	this.cartItemList = cartItemList;
+}
+
+public long getAddrId() {
+	return addrId;
+}
+
+public void setAddrId(long addrId) {
+	this.addrId = addrId;
+}
+
+public String getBuyerComment() {
+	return buyerComment;
+}
+
+public void setBuyerComment(String buyerComment) {
+	this.buyerComment = buyerComment;
 }
 }
